2f49fb27b9cd3332c5c8a9e5a82d4e63cfaedba1
jile0810 <jimbo.jile10@gmail.com>
2018-02-10 21:35:19 +0800

    offerings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
 import { AboutComponent } from './component/about/about.component';
 import { ContactComponent } from './component/contact/contact.component';
+import { OfferingComponent } from './component/offering/offering.component';
 
 const routes: Routes = [
 	{ path: '', redirectTo: 'home', pathMatch: 'full'},
 	{ path: 'home', component: HomeComponent },
 	{ path: 'about', component: AboutComponent },
-	{ path: 'contact', component: ContactComponent }
+	{ path: 'contact', component: ContactComponent },
+	{ path: 'offering', component: OfferingComponent }
 ];
 
 @NgModule({
@@ -16,5 +18,5 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [HomeComponent,AboutComponent,ContactComponent];
+export const routingComponents = [HomeComponent,AboutComponent,ContactComponent,OfferingComponent];
 
