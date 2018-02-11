dbd9d8c7c6ee38cd7829b6fe01e26d88d3a390f3
jile0810 <jimbo.jile10@gmail.com>
2018-02-12 00:23:45 +0800

    working contact form and messaging

--- src/app/component/contact/contact.component.ts
+++ src/app/component/contact/contact.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { NgModule }           from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 
+declare let emailjs:any;
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
+@NgModule({
+  imports: [
+  	FormsModule      //<----------make sure you have added this.
+   ],
+})
 export class ContactComponent implements OnInit {
+  constructor(){
 
-  constructor() { }
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit(contactForm: any){
+  	console.log(contactForm.value);
+  	var service_id = "default_service";
+  	var template_id = "template_4WwK1vLp";
+  	emailjs.send(service_id,template_id,contactForm.value)
+  	.then(function(){ 
+    	alert("Sent!");
+    }, function(err) {
+       alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+    });
+  	contactForm.reset();
   }
 
 }
