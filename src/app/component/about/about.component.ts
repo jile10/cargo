2711d107aa26b2802da48aac180bbc47b59a737b
jile10 <jimbo.jile10@gmail.com>
2018-02-22 12:12:59 +0800

    fix carousel

--- src/app/component/about/about.component.ts
+++ src/app/component/about/about.component.ts
@@ -11,7 +11,7 @@ export class AboutComponent implements OnInit {
 
   ngOnInit() {
   	$('.carousel').carousel({
-	  interval: 2000
+	  interval: 3000
 	})
   }
 
--- src/app/component/offering/offering.component.ts
+++ src/app/component/offering/offering.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $:any;
 @Component({
   selector: 'app-offering',
   templateUrl: './offering.component.html',
@@ -10,6 +10,16 @@ export class OfferingComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  	$( document ).ready(function() {
+	    $(".setsize").each(function() {
+	        $(this).height($(this).width());
+	    });
+	});
+	$(window).on('resize', function(){
+	    $(".setsize").each(function() {
+	        $(this).height($(this).width());
+	    });
+	});
   }
 
 }
